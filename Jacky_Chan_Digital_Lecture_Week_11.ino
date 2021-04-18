4d3179efcb075bae2ef63cc29947e8f1ad058c50
JackyChanNYU <82749519+JackyChanNYU@users.noreply.github.com>
2021-04-18 15:48:42 +0800

    added an on/off button

--- Jacky_Chan_Digital_Lecture_Week_11.ino
+++ Jacky_Chan_Digital_Lecture_Week_11.ino
@@ -1,16 +1,23 @@
 int ledPin = 8;
 int ledPin2 = 9;
+int buttonPin = 35;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(ledPin2, OUTPUT);
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH);
-  digitalWrite(ledPin2, HIGH);
-  delay(500);
-  digitalWrite(ledPin, LOW);
-  digitalWrite(ledPin2, LOW);
-  delay(500);
+  if (digitalRead(buttonPin) == HIGH) {
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(ledPin2, HIGH);
+    delay(500);
+    digitalWrite(ledPin, LOW);
+    digitalWrite(ledPin2, LOW);
+    delay(500);
+  } else {
+    digitalWrite(ledPin, LOW);
+    digitalWrite(ledPin2, LOW);
+  }
 }
